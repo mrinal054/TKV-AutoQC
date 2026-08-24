3631f3165388e34af9084358a2864dacdd5c1eee
Abigail Green <abgreen03@gmail.com>
2026-08-24 09:01:04 -0500

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,8 +1,27 @@
+# ResNet-based
+from networks.resnet import ResNetClassifier, ResNetFeatures
+from networks.densenet import DenseNetClassifier, DenseNetFeatures
 from networks.medicalnet_resnet18 import MedicalNetResNet18Classifier, MedicalNetResNet18FeaturesV2
+
+# VGG-based
+from networks.vgg16 import VGG16Classifier, VGG16Features3DV2
+from networks.vgg16_multitask import VGG16MultiTask
+
+# CLIP-based
+from networks.ctclip import ImageLatentsClassifier
+from networks.ctclip_v2 import ImageLatentsClassifierV2
+
+# Others
 from networks.multiImageNet import MultiImageNet
+from networks.densenet import DenseNetClassifier, DenseNetFeatures
+from networks.vit import ViTClassifier, ViTFeatures
+from networks.swin_unetr import SwinUNETRClassifier, SwinUNETRFeatures
+from networks.efficientnet import EffNetClassifier, EffNetFeatures
 
 # Expose models at the module level
-__all__ = ['MedNetResNet18Classifier', 'MedicalNetResNet18FeaturesV2', 'MultiImageNet']
+__all__ = ['ResNetClassifier', 'ResNetFeatures', 'DenseNetClassifier', 'DenseNetFeatures', 'VGG16Classifier', 'VGG16Features3DV2', 'ViTClassifier', 'ViTFeatures', 'SwinUNETRClassifier', 'SwinUNETRFeatures', 'EffNetClassifier', 'EffNetFeatures', 'MedNetResNet18Classifier', 'MultiImageNet', 'MedicalNetResNet18FeaturesV2']
+
+
 
 
 

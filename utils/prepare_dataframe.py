71d49daba96992f5203c0e5b292aed0a94ed3443
Abigail Green <abgreen03@gmail.com>
2026-08-24 09:20:05 -0500

    Update prepare_dataframe.py

--- utils/prepare_dataframe.py
+++ utils/prepare_dataframe.py
@@ -43,28 +43,6 @@ def prep_df_multiCls(directories: list, labels: list, str_labels: list = None,
 
     df = pd.DataFrame(all_rows)
 
-    all_rows = []
-
-    for i, dir in enumerate(directories):
-        names = os.listdir(dir)
-
-        if ends_with is not None:
-            names = [name for name in names if name.endswith(ends_with)]
-
-        fullfiles = [os.path.join(dir, name) for name in names]
-
-        label_repeated = [labels[i]] * len(fullfiles)
-
-        if str_labels is not None:
-            str_label_repeated = [str_labels[i]] * len(fullfiles)
-            for n, f, l, sl in zip(names, fullfiles, label_repeated, str_label_repeated):
-                all_rows.append({"Names": n, "Directories": f, "Labels": l, "Str_labels": sl})
-        else:
-            for n, f, l in zip(names, fullfiles, label_repeated):
-                all_rows.append({"Names": n, "Directories": f, "Labels": l})
-
-    df = pd.DataFrame(all_rows)
-
     if save_fullfile is not None:
         df.to_excel(save_fullfile, index=False)
 
@@ -72,19 +50,6 @@ def prep_df_multiCls(directories: list, labels: list, str_labels: list = None,
 
 
 if __name__ == "__main__":
-    
-    directories = [
-        '/scratch/abbydev/dataset/stratified_split_v1/test/Accept',
-        '/scratch/abbydev/dataset/stratified_split_v1/test/Reject',
-        '/scratch/abbydev/dataset/stratified_split_v1/test/Rework'
-    ]
-
-    labels = [0, 1, 2]
-    str_labels = ["Accept", "Reject", "Rework"]
-    dir_excel_save = "/research/m324371/Project/Digital_Twin/Classification/Dataframes/AccRejRew_test.xlsx"
-
-    df = prep_df_multiCls(directories = directories, 
-                          labels = labels, 
-                          str_labels = str_labels, 
-                          ends_with = ".nii.gz", 
-                          save_fullfile = dir_excel_save)
+    raise SystemExit(
+        "Import prep_df_multiCls() and provide your own directory and output paths."
+    )

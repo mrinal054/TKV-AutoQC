17ada9d55608b4715e965ae03e7a5931d85adc76
Abigail Green <abgreen03@gmail.com>
2026-08-24 09:46:28 -0500

    Update loss.py

--- losses/loss.py
+++ losses/loss.py
@@ -1,9 +1,13 @@
 import torch.nn as nn
 
-def loss_func(name, weight=None, *args):
-    if name == 'ce':
-        if weight == None: return nn.CrossEntropyLoss()
-        else: return nn.CrossEntropyLoss(weight=weight)
+def loss_func(name, weight=None, label_smoothing=0.0, *args):
+    if name == "ce":
+        if weight is None:
+            return nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+        return nn.CrossEntropyLoss(
+            weight=weight,
+            label_smoothing=label_smoothing,
+        )
     elif name == 'bce':
         if weight == None: return nn.BCEWithLogitsLoss()
         else: return nn.BCEWithLogitsLoss(pos_weight=weight)
